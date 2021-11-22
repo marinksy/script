0da49d12d6046860643d260fb590f1e2f29318aa
adrianmxh <94862625+adrianmxh@users.noreply.github.com>
2021-11-22 21:38:09 +0200

    Update mainframe.sh

--- mainframe.sh
+++ mainframe.sh
@@ -152,7 +152,7 @@ sed -i '/allow_url_fopen = /c\allow_url_fopen = On' /opt/cpanel/ea-php*/root/etc
 sed -i '/allow_url_include = /c\allow_url_fopen = On' /opt/cpanel/ea-php*/root/etc/php.ini
 sed -i '/memory_limit/c\memory_limit = 2048M' /opt/cpanel/ea-php*/root/etc/php.ini
 
-echo 'Instalarea a fost efectuata cu success!!!Urmeaza reboot final.pwp'
+echo 'Instalarea a fost efectuata cu success!!! Repornirea serverului este in progres'
 ########load avarage din w cu print pe char row 10/11/12#####
 echo 'Loadul actual este' && w | awk '/load average/ { printf "%s %s %s\n", $10, $11, $12 }'
 
