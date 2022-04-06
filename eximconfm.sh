effd9a12a29b85291a7e842fcc82a8221f395e89
Adrian Rus <94862625+adrianmxh@users.noreply.github.com>
2022-04-06 12:45:07 +0300

    Update eximconfm.sh

--- eximconfm.sh
+++ eximconfm.sh
@@ -7,6 +7,7 @@ sed -i '/acl_spamhaus_rbl/c\acl_spamhaus_rbl=1' /etc/exim.conf.localopts
 sed -i '/use_rdns_for_helo/c\use_rdns_for_helo=0' /etc/exim.conf.localopts
 sed -i '/custom_mailhelo/c\custom_mailhelo=1' /etc/exim.conf.localopts
 sed -i '/custom_mailips/c\custom_mailips=1' /etc/exim.conf.localopts
+sed -i '/use_rdns_for_helo/c\use_rdns_for_helo=0' /etc/exim.conf.localopts
 touch /var/cpanel/custom_mailips
 touch /var/cpanel/custom_mailhelo
 sed -i '/require_secure_auth/c\require_secure_auth=0' /etc/exim.conf.localopts
