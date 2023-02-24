5b8e7e854bc6a2eed0d2e8f99f5cf694692bc4d1
Adrian Rus <94862625+adrianmxh@users.noreply.github.com>
2023-02-24 15:32:33 +0200

    Update mainframe.sh

--- mainframe.sh
+++ mainframe.sh
@@ -34,12 +34,12 @@ sed -i '/SELINUX=permissive/c\SELINUX=disabled' /etc/selinux/config
 
 #####Selectare Stable tier####
 touch /etc/cpupdate.conf
-echo "CPANEL=stable" > /etc/cpupdate.conf
+echo "CPANEL=release" > /etc/cpupdate.conf
 #####Final selectare Stable tier####
 #####Selectare MariaDB 10.6####
-#mkdir /root/cpanel_profile
-#touch /root/cpanel_profile/cpanel.config
-#echo "mysql-version=10.6" > /root/cpanel_profile/cpanel.config
+mkdir /root/cpanel_profile
+touch /root/cpanel_profile/cpanel.config
+echo "mysql-version=10.6" > /root/cpanel_profile/cpanel.config
 #####Final selectare MariaDB 10.6####
 
 cd /home
