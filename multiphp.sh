a109154cd450041f24cff8acb9bcbc7ca3bb4661
marinksy <156511667+marinksy@users.noreply.github.com>
2024-02-10 09:09:02 +0200

    Update multiphp.sh

--- multiphp.sh
+++ multiphp.sh
@@ -6,7 +6,7 @@ modify_php_config() {
     local config_file="/opt/cpanel/ea-php${php_version}/root/etc/php.ini"
 
     sed -i '/disable_functions =/c\disable_functions = \"exec,passthru,shell_exec,system,proc_open,popen,show_source\"' $config_file
-    sed -i '/open_basedir =/c\open_basedir = \"\/home:\/usr\/lib\/php:\/usr\/local\/lib\/php:\/tmp:\/var\/cpanel\/php\/sessions\/ea-php'"${php_version}$
+    sed -i '/open_basedir =/c\open_basedir = \"\/home:\/usr\/lib\/php:\/usr\/local\/lib\/php:\/tmp:\/var\/cpanel\/php\/sessions\/ea-php'"${php_version}"':\/usr\/local\/lsws\"' $config_file
     sed -i '/max_execution_time/c\max_execution_time = 3600' $config_file
     sed -i '/max_input_time/c\max_input_time = 3600' $config_file
     sed -i '/max_input_vars/c\max_input_vars = 10000' $config_file
@@ -21,6 +21,6 @@ modify_php_config() {
 }
 
 # Loop through PHP versions including 5.6, 7.4, and 8.0 to 8.3
-for php_version in 56 74 {80..83}; do
+for php_version in 54 55 56 {70..74} {80..83}; do
     modify_php_config "$php_version"
 done
